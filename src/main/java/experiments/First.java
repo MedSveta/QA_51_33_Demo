2368eb4fb6991e1c9f4c6c5c8dd30f3ce0137636
MedSveta <sveta1978medved@gmail.com>
2026-04-19 19:20:32 +0300

    Fisrt Class

--- src/main/java/experiments/First.java
+++ src/main/java/experiments/First.java
@@ -1,4 +1,35 @@
 package experiments;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class First {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void firstTest() {
+        driver.get("https://demoqa.com/");
+        pause(5000);
+        driver.manage().window().maximize();
+        pause(5000);
+        driver.get("https://demoqa.com/elements");
+        pause(5000);
+        driver.navigate().back();
+        pause(5000);
+        driver.navigate().forward();
+        pause(5000);
+        driver.navigate().refresh();
+        pause(5000);
+        //driver.close();
+        driver.quit();
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
